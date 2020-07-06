1ee11ee4635ceabe23bd799c590d8cb8eaad9f53
Radhika Gupta <gupta.radhika61997@gmail.com>
2020-07-06 21:39:55 +0530

    blog app updates

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
+posts = [
+    {
+        'author': 'Radhika Gupta',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 27,2020'
+
+    },
+    {
+        'author': 'Prakhar Gupta',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 28,2020'
+
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+    return render(request, 'blog/about.html',{'title': "About"})
