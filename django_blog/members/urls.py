bf54a741b50860b75fb6b5d0ca444c8ff945729c
Radhika Gupta <gupta.radhika61997@gmail.com>
2020-07-17 00:11:09 +0530

    edit profile form added

--- django_blog/members/urls.py
+++ django_blog/members/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
    path('register/',UserRegistrationView.as_view(), name='register' ),
+   path('edit_profile/',UserEditView.as_view(), name='edit_profile' ),
 ]
