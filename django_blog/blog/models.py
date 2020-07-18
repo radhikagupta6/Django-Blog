cc7a904f3d0da489cc10fe37e930cd3d93926b91
Radhika Gupta <gupta.radhika61997@gmail.com>
2020-07-18 23:17:29 +0530

    edit profile

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -7,8 +7,21 @@ from datetime import datetime, date
 from ckeditor.fields import RichTextField
 # Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)    
+    bio = models.TextField()
+    profile_pic = models.ImageField(null= True, blank = True ,upload_to="uploads/prolfile", default='e.png')
+    website_url = models.CharField(max_length= 255, null=True, blank=True)
+    facebook_url = models.CharField(max_length= 255, null=True, blank=True)
+    linkedn_url = models.CharField(max_length= 255, null=True, blank=True)
+    twitter_url = models.CharField(max_length= 255, null=True, blank=True)
+    github_url = models.CharField(max_length= 255, null=True, blank=True)
     
-
+    def __str__(self):
+        return str(self.user)
+    
+    
+    
 class Post(models.Model):
     title = models.CharField(max_length= 255, null=False, blank=False)
     title_tag = models.CharField(max_length= 255)
