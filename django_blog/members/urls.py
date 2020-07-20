b5e03916ecf8b192787c5a9f3012599b588af5f1
Radhika Gupta <gupta.radhika61997@gmail.com>
2020-07-21 01:22:05 +0530

    profile page added

--- django_blog/members/urls.py
+++ django_blog/members/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
    path('edit_profile/',UserEditView.as_view(), name='edit_profile' ),
    path('password/',PasswordsChangeView.as_view(template_name = 'registration/change-password.html')),
    # path('password/',auth_views.PasswordChangeView.as_view(template_name = 'registration/change-password.html')),
-   path('password_success', views.password_success, name='password_success')
+   path('password_success', views.password_success, name='password_success'),
+   path('<int:pk>/profile/', ShowProfilePageView.as_view(), name= 'show_profile_page'),
 ]
